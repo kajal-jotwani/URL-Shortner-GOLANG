1a04ac3f919b37c612691f215417729752fc6792
kajal-jotwani <kajaljotwani06@gmail.com>
2025-12-22 18:33:43 +0530

    fix typo in fiber package name and update dependencies

--- api/main.go
+++ api/main.go
@@ -5,12 +5,12 @@ import (
 	"log"
 	"os"
 
-	"github.com/gofibre/fibre/v2"
-	"github.com/gofibre/fibre/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/joho/godotenv"
 )
 
-func setupRoutes(app *fibre.App) {
+func setupRoutes(app *fiber.App) {
 	app.Get("/:url", routes.ResolveURL)
 	app.Post("/api/v1", routes.ShortenURL)
 }
