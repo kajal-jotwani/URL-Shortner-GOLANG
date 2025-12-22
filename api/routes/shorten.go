7235f244ce66c870c1d9d5367b391d75027acc47
kajal-jotwani <kajaljotwani06@gmail.com>
2025-12-22 18:18:08 +0530

    feat: add logic for URL shortening, validation, and helper utilities

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -2,6 +2,8 @@ package routes
 
 import (
 	"time"
+
+	"github.com/asaskevich/govalidator"
 )
 
 type request struct {
@@ -16,4 +18,30 @@ type response struct {
 	Expiry          time.Duration `json:"expiry"`
 	XRateRemaining  int           `json:"rate_limit"`
 	XRateLimitReset time.Duration `json:"rate_limit_reset"`
+}
+
+func ShortenURL(c *fiber.Ctx) error {
+
+	body := new(request)
+
+	if err := c.BodyParser(&body); err != nil {
+		return c.Status(fibre.StatusBadRequest).JSON(fibre.Map{"error": "cannot parse JSON"})
+	}
+
+	//implement rate limiting
+	//check if the input is an actual url
+
+	if !govalidator.IsURL(body.URL) {
+		return c.Status(fibre.StatusBadRequest).JSON(fibre.Map{"error": "Invalid URL"})
+	}
+
+	//check for the domain error
+
+	if !helpers.RemoveDomainError(body.URL) {
+		return c.Status(fibre.StatusServiceUnavailable).JSON(fibre.Map{"error": "You can't hack the system!!"})
+	}
+
+	//enforce https, SSL
+
+	body.URL = helpers.EnforceHTTP(body.URL)
 }
